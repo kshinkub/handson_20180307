697817e5ba997e63acd9109344105f6a9097871a
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-02-19 11:57:38 +0900

    Create create_column_table.sql

---
+++ create_column_table.sql
@@ -0,0 +1,26 @@
+create COLUMN TABLE books(
+    id BIGINT,
+    isbn VARCHAR(128),
+    category VARCHAR(128),
+    publish_date TIMESTAMP,
+    publisher VARCHAR(128),
+    price FLOAT
+);
+
+create COLUMN TABLE customers(
+    id BIGINT,
+    name VARCHAR(128),
+    date_of_birth TIMESTAMP,
+    gender VARCHAR(128),
+    state VARCHAR(128),
+    email VARCHAR(256),
+    phone VARCHAR(128)
+);
+
+create COLUMN TABLE transactions(
+    id BIGINT,
+    customer_id BIGINT,
+    book_id BIGINT,
+    quantity INT,
+    transaction_date TIMESTAMP
+);
