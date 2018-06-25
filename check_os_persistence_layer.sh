19bb5971167dac4f4e93b9170d90f83120b49961
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-06-25 14:08:42 +0900

    Create check_os_persistence_layer.sh

---
+++ check_os_persistence_layer.sh
@@ -0,0 +1,1 @@
+ls --full-time -tr /hana/shared/log/HXE/mnt00001/hdb00002.00003/logsegment_000_* | grep -v directory | tail -n 1 | awk '{print $6 $7 $8 $9}' ; ls --full-time /hana/shared/data/HXE/mnt00001/hdb00002.00003/datavolume_0000.dat | awk '{print $6 $7 $8 $9}'
