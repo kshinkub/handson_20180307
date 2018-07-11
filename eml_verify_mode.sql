d61a5ba7537b5846867a6aa1b6bd0a481f5f13b1
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-07-11 13:53:55 +0900

    Update eml_verify_mode.sql

--- eml_verify_mode.sql
+++ eml_verify_mode.sql
@@ -1,3 +1,3 @@
 -- verify model is up and running on remote source
-INSERT INTO "PARAMETERS" VALUES ('Model', 'mnist%predict_images');
+INSERT INTO "PARAMETERS" VALUES ('Model', 'mnist');
 CALL "_SYS_AFL"."EML_CHECKDESTINATION_PROC" ("PARAMETERS", ?);
