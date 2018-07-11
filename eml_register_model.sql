616fb4ab5e341c937f3497e8fdc067a7c92c02c5
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-07-11 13:53:34 +0900

    Update eml_register_model.sql

--- eml_register_model.sql
+++ eml_register_model.sql
@@ -1,6 +1,6 @@
 -- register model
-DELETE FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION" WHERE "Model"='mnist%predict_images';
+DELETE FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION" WHERE "Model"='mnist';
 INSERT INTO "_SYS_AFL"."EML_MODEL_CONFIGURATION" ("Model", "Parameter", "Value") 
-VALUES ('mnist%predict_images', 'RemoteSource', 'TensorFlowModelServer');
+VALUES ('mnist', 'RemoteSource', 'TensorFlowModelServer');
 
 SELECT * FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION";
