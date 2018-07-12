020b9af5fd8c758d6909b4d0dad6c807feaf64f8
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-07-12 16:02:19 +0900

    Create img2ascii.sh

---
+++ img2ascii.sh
@@ -0,0 +1,10 @@
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_0_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_1_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_2_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_3_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_4_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_5_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_6_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_7_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_8_001.jpg
+python /tmp/sample/scripts/img2ascii.py /tmp/sample/test/tf/sample_9_001.jpg
