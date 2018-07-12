a632ceadd38ecb2637848944185d8fccba399e1d
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-07-12 11:36:52 +0900

    Update eml_apply_registered_model.sql

--- eml_apply_registered_model.sql
+++ eml_apply_registered_model.sql
@@ -1,5 +1,2 @@
 -- create parameters table (used in subsequent calls)
 CREATE TABLE "PARAMETERS" ("Parameter" VARCHAR(100), "Value" VARCHAR(100));
-
--- apply registered models
-CALL "_SYS_AFL"."EML_CTL_PROC" ('UpdateModelConfiguration', "PARAMETERS", ?);
