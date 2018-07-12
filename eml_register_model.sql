ad93362e1eed572b30d5c500d55d921130981cb6
kshinkub <36613597+kshinkub@users.noreply.github.com>
2018-07-12 11:27:47 +0900

    Update eml_register_model.sql

--- eml_register_model.sql
+++ eml_register_model.sql
@@ -1,6 +1,7 @@
 -- register model
-DELETE FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION" WHERE "Model"='mnist';
+DELETE FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION" WHERE "Model"='mnist%serving_default';
+---- Model : model%signature (default signature is "serving_default")
 INSERT INTO "_SYS_AFL"."EML_MODEL_CONFIGURATION" ("Model", "Parameter", "Value") 
-VALUES ('mnist', 'RemoteSource', 'TensorFlowModelServer');
+VALUES ('mnist%serving_default', 'RemoteSource', 'TensorFlowModelServer');
 
 SELECT * FROM "_SYS_AFL"."EML_MODEL_CONFIGURATION";
